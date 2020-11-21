fdfc5a7c35eae0dff5afee3abee59f768cdcfd90
mrransed <ransedweb@gmail.com>
2020-11-21 14:58:55 -0500

    primer base del proiyecto

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="./fontawesome/css/all.css" rel="stylesheet"> 
+    <link href="./css/bootstrap-grid.css" rel="stylesheet"> 
+    
+
+</head>
+<body>
+
+    <header>
+            
+        <div class="container">
+            <div class="row justify-content-center align-items-center">
+
+                <div class="col-auto">
+                    <h1>myordernow</h1>
+                </div>
+                <div class="col-auto">
+                    <div class="avatar-container">
+                        <img src="./img/avatar.jpg" alt="">
+                    </div>
+                </div>
+
+
+
+            
+            </div>
+        </div>
+    </header>
+
+    <script src="./js/jquery.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+</body>
+</html>
